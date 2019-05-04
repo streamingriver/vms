d14fb351a84748a5833279107d96b15115dddecd
Mindaugas Žvirblis <tinklapiai@gmail.com>
2019-05-04 22:18:17 +0300

    database schemas

---
+++ db.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS `channels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `url` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `stream_url` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `channels_packages` (
+  `channel_id` int(11) NOT NULL,
+  `package_id` int(11) NOT NULL,
+  UNIQUE KEY `channel_id` (`channel_id`,`package_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` text NOT NULL,
+  `mac` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `package_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+CREATE TABLE IF NOT EXISTS `packages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
