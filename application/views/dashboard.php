9a37c432c083527026b86e927344c105fbeb5b42
Mindaugas Žvirblis <tinklapiai@gmail.com>
2019-05-13 09:52:23 +0300

    dashboard texts

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -7,6 +7,10 @@
                 1: Create packages<br/>
                 2: Create Channel and press "add" on right side of channels to add channel to package<br/>
                 3: Add client. After you add client, you see url for client to m3u format playlist<br/>
+                4: After changing urls in channels, dont forget to press "update backend" button<br/> 
+                <br/>
+                <br/>
+                If you have any questions or feature requests please contact us on support@streamingriveriptv.com
                 </h3>
             </div>
         </div>
